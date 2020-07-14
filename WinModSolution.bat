8b5960bffd42fe972137d817719f93fd73021cd7
Calvin Bruce <36201282+calvin5008@users.noreply.github.com>
2020-07-14 10:07:50 -0700

    Add files via upload

---
+++ WinModSolution.bat
@@ -0,0 +1,92 @@
+@echo off
+
+
+set/a num=(%Random% %%9)+1
+
+color %num%
+
+echo                    888               .o@8    
+echo                    888           .o@8888%    
+echo                    888       .o@8888%"       
+echo               8888888888888  8888R"           
+echo               8888888888888  "*8888bu.        
+echo                    888         ^"*8888bu.   
+echo                    888             ^%*888    
+echo                    888                 ^%
+
+              #Coded By Calvin_Bruce
+
+echo               #Don't Dare To MOd IT
+echo        #Essential Module Downloading Script
+ echo                   #For Windows Only
+
+echo.........
+echo........
+echo............
+echo Connected to network
+break;
+echo Use good internet connection for best speed
+break;
+break;
+break;
+echo Highly recommand for use (python 2.7.17 version)
+
+echo Downloading....................................................................
+echo ..............................................................................
+
+echo................................. colorama downloading.....................................
+
+cd c:\Python27\Scripts
+pip install colorama
+
+echo Another File downloading......................
+echo .........................................
+echo................................. bs4 downloading.............................................
+pip install bs4
+echo Another File downloading...........................................................................
+echo ...................................................................................................
+echo ................................Modules downloading.................................................
+pip install modules
+
+
+
+
+echo Another Downloading.................................
+echo.................................Requests downloading..................................................
+pip install requests
+pip install request
+
+echo Another Downloading................................
+echo.................................http_request_randomizer................................................
+pip install http_request_randomizer
+
+echo Another Downloading..................................
+echo..................................requests.proxy........................................................
+pip install requests.proxy
+@echo off
+echo..................ALL Are Complete.....................
+echo ....................IF you Face any problem Contact ME............
+echo................https://www.facebook.com/calvin.bruce5008 .....
+echo.................Thanks For using This tool...........
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pause
